348c990078ba0b4841b2095b6217a4608f33eb76
manos <savemanos@gmail.com>
2015-02-23 21:34:44 +0100

    Still trying basic stuff

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from libcnml import CNMLParser, Status
 import pyGuifiAPI
 from pyGuifiAPI.error import GuifiApiError
 
-from configmanager import GuifinetStudioConfig
+#from configmanager import GuifinetStudioConfig
 
 from utils import *
 
@@ -26,26 +26,24 @@ class GuifiNet:
 		try:
 			gui.auth()
 		except GuifiApiError, e:
-		     print e.reason
+			print e.reason
 		print self.gui.is_authenticated()
 		print self.gui.authToken
 
-
-
 		if not cnmlFile:
-            # Load default zone cnml
-            try:
-	            fp = self.gui.downloadCNML(GUIFI_NET_WORLD_ZONE_ID, 'zones')
-	            filename = os.path.join(os.getcwd(),'cnml','GUIFI_NET_WORLD_ZONE_ID')
-	            with open(filename, 'w') as zonefile:
-	                zonefile.write(fp.read())
-	            print _('Zone saved successfully to'), filename
-	            self.cnmlFile = filename
-        	except URLError, e:
-	            print _('Error accessing to the Internet:'), str(e.reason)
+			# Load default zone cnml
+			try:
+				fp = self.gui.downloadCNML(GUIFI_NET_WORLD_ZONE_ID, 'zones')
+				filename = os.path.join(os.getcwd(),'cnml','GUIFI_NET_WORLD_ZONE_ID')
+				with open(filename, 'w') as zonefile:
+					zonefile.write(fp.read())
+				print _('Zone saved successfully to'), filename
+				self.cnmlFile = filename
+			except URLError, e:
+				print _('Error accessing to the Internet:'), str(e.reason)
 
         if cnmlFile:
-            # CNMLParser
+        # CNMLParser
             try:
                 self.cnmlp = CNMLParser(cnmlFile)
                 self.cnmlFile = cnmlFile
@@ -55,16 +53,19 @@ class GuifiNet:
                 print _('Error opening CNML file')
 
         try:
-            self.zonecnmlp = CNMLParser(cnmlGWfile)
+            #self.zonecnmlp = CNMLParser(cnmlFile)
+            self.zonecnmlp = cnmlp
             for z in self.zonecnmlp.getZones():
+            	print _('Zone id:'), z.id
+            	print _('Zone Title:'), z.title
                 self.allZones.append((z.id, z.title))
         except IOError:
             print _('Error loading cnml guifiworld zone:'), cnmlFile
             self.zonecnmlp = None
 
 
-
-
-		# CNML
-		
-		
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        GuifiNet(sys.argv[1])
+    else:
+        GuifiNet()
