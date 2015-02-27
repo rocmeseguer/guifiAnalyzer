7497430daee9b8da588501b5caa6c829dd27061c
manos <savemanos@gmail.com>
2015-02-27 20:53:57 +0100

    Making it a bit more interactive

--- test.py
+++ test.py
@@ -37,69 +37,90 @@ class GuifiNet:
         print self.gui.is_authenticated()
         print self.gui.authToken
 
-        if not cnmlFile:
-            # Load default zone cnml
-            print "Download World CNML"
-            try:
-                fp = self.gui.downloadCNML(GUIFI_NET_WORLD_ZONE_ID, 'zones')
-                filename = os.path.join(os.getcwd(),'cnml','GUIFI_NET_WORLD_ZONE_ID')
-                with open(filename, 'w') as zonefile:
-                    zonefile.write(fp.read())
-                print _('Zone saved successfully to'), filename
-                self.cnmlFile = filename
-            except URLError, e:
-                print _('Error accessing to the Internet:'), str(e.reason)
 
-        if cnmlFile:
-            print "Using indicated CNML file"
-            self.cnmlFile = cnmlFile
 
-        # CNMLParser
-        print "CNML Parser"
+    #     if not cnmlFile:
+    #         # Load default zone cnml
+    #         print "Download World CNML"
+    #         try:
+    #             fp = self.gui.downloadCNML(GUIFI_NET_WORLD_ZONE_ID, 'zones')
+    #             filename = os.path.join(os.getcwd(),'cnml',str(GUIFI_NET_WORLD_ZONE_ID))
+    #             with open(filename, 'w') as zonefile:
+    #                 zonefile.write(fp.read())
+    #             print _('Zone saved successfully to'), filename
+    #             self.cnmlFile = filename
+    #         except URLError, e:
+    #             print _('Error accessing to the Internet:'), str(e.reason)
+
+    #     if cnmlFile:
+    #         print "Using indicated CNML file"
+    #         self.cnmlFile = cnmlFile
+
+
+    # def saveZones(self):
+    #     # CNMLParser
+    #     print "CNML Parser"
+    #     try:
+    #         self.cnmlp = CNMLParser(self.cnmlFile)
+    #         try:
+    #             #self.zonecnmlp = CNMLParser(cnmlFile)
+    #             self.zonecnmlp = self.cnmlp
+    #             for z in self.zonecnmlp.getZones():
+    #                 #print _('Zone id:'), z.id
+    #                 #print _('Zone Title:'), z.title
+    #                 self.allZones.append((z.id, z.title))
+    #         except IOError:
+    #             print _('Error loading cnml guifiworld zone:'), self.cnmlFile
+    #             self.zonecnmlp = None
+    #     except IOError:
+    #         self.cnmlp = None
+    #         self.cnmlFile = None
+    #         print _('Error opening CNML file')
+
+
+    def parseZoneCNML(self,zone):
+        zonefile = os.path.join(os.getcwd(),'cnml',str(zone))
+        if not os.path.isfile(zonefile):
+            print "Cannot find zone locally. Will download"
+            zonefile = self.getZoneCNML(zone)
+
         try:
-            self.cnmlp = CNMLParser(self.cnmlFile)
-            try:
-                #self.zonecnmlp = CNMLParser(cnmlFile)
-                self.zonecnmlp = self.cnmlp
-                for z in self.zonecnmlp.getZones():
-                    #print _('Zone id:'), z.id
-                    #print _('Zone Title:'), z.title
-                    self.allZones.append((z.id, z.title))
-            except IOError:
-                print _('Error loading cnml guifiworld zone:'), self.cnmlFile
-                self.zonecnmlp = None
-        except IOError:
-            self.cnmlp = None
-            self.cnmlFile = None
-            print _('Error opening CNML file')
+            return CNMLParser(zonefile)
+        except  IOError:
+            print _('Error opening CNML file: ' ), zonefile
 
+    def getZoneCNML(self,zone):
         # Download zone cnml and get links
         print "Get links and their nodes of a zone"
-        zone_id = raw_input("Select a zone: ")
+        if  not zone:
+            zone = raw_input("Select a zone: ")
         try:
-            fp = self.gui.downloadCNML(int(zone_id), 'zones')
-            filename = os.path.join(os.getcwd(),'cnml',zone_id)
+            fp = self.gui.downloadCNML(int(zone), 'zones')
+            filename = os.path.join(os.getcwd(),'cnml',str(zone))
             with open(filename, 'w') as zonefile:
                 zonefile.write(fp.read())
             print _('Zone saved successfully to'), filename
-            zonefile = filename
+            return filename
         except URLError, e:
             print _('Error accessing to the Internet:'), str(e.reason)
 
-        # Find the relevant links
-        print "Find the relevant links"
-        try:
-            zonecnmlp = CNMLParser(zonefile)
-            try:
-                for z in zonecnmlp.getLinks():
-                    print "Found a link"
-                    print _('Link id:'), z.id
-                    print _('Node A:'), z.nodeA
-                    print _('Node B:'), z.nodeB
-            except IOError:
-                print _('Error loading cnml guifiworld zone:'), self.cnmlFile
-        except IOError:
-            print _('Error opening CNML file')
+
+
+
+        # # Find the relevant links
+        # print "Find the relevant links"
+        # try:
+        #     zonecnmlp = CNMLParser(zonefile)
+        #     try:
+        #         for z in zonecnmlp.getLinks():
+        #             print "Found a link"
+        #             print _('Link id:'), z.id
+        #             print _('Node A:'), z.nodeA
+        #             print _('Node B:'), z.nodeB
+        #     except IOError:
+        #         print _('Error loading cnml guifiworld zone:'), self.cnmlFile
+        # except IOError:
+        #     print _('Error opening CNML file')
 
 
 if __name__ == "__main__":
