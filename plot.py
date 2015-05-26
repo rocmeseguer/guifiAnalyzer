211fb14cadcd9a92ae86a1b2230e9a05aab3b806
manos <savemanos@gmail.com>
2015-05-26 20:58:41 +0200

    Improving plots

--- plot.py
+++ plot.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 #from matplotlib import pyplot as plt
 from matplotlib.dates import date2num
 
+from statsmodels.distributions.empirical_distribution import ECDF
+
 from collections import Counter
 
 
@@ -18,23 +20,91 @@ root = 2413
 g = CNMLWrapper(root)
 
 
-nodeD = [n.totalLinks for n in g.nodes.values()]
-data = Counter(nodeD).items()
-data = sorted(data,key=lambda x: x[0])
+user = ['meteo',  'radio', 'web','VPS','tv','wol', 'Proxy', 'mail','irc',
+                            'teamspeak', 'ftp' ,'asterisk', 'apt-cache','AP','IM', 'p2p',
+                            'VPN','Streaming','games','cam']
+mgmtServices = ['iperf', 'LDAP','DNS','SNPgraphs','NTP','AirControl']
 
-x = [degree for (degree, freq) in data]
-y = [freq for (degree,freq) in data]
-y = np.cumsum(y)
+userServices = [s.type for s in g.services.values() if s.type in user]
+totalServices = len(userServices)
+userServices = Counter(userServices).items()
+userServicesNumber = len(userServices)
+types = [typ for (typ,values) in userServices]
+values = [float(value)/float(totalServices) for (typ,value) in userServices]
 
-plt.scatter(x, y)
+
+ind = np.arange(userServicesNumber)
+width = 0.35
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+rects = ax.bar(ind,values, width, color='black')
+ax.set_xlim(-width,len(ind)+width)
+#ax.set_ylim(0,45)
+ax.set_ylabel('Frequency')
+ax.set_xlabel('Service Type')
+ax.set_title('User Services Frequency')
+xTickMarks = [str(i) for i in types]
+ax.set_xticks(ind+width)
+xtickNames = ax.set_xticklabels(xTickMarks)
+plt.setp(xtickNames, rotation=45, fontsize=10)
+
+## add a legend
+#ax.legend( (rects1[0], rects2[0]), ('Men', 'Women') )
+
 plt.show()
 
 
 
-def creationDateECDF():
+def nodeDegreeECDF():
+    nodeD = [n.totalLinks for n in g.nodes.values()]
+    ecdf = ECDF(nodeD)
+
+    # Initilaze figure to print later
+    fig = plt.Figure()
+    fig.set_canvas(plt.gcf().canvas)
+    plt.plot(ecdf.x, ecdf.y)
+    # Set graph parameters
+    plt.title("Node Degree ECDF")
+    plt.ylabel("Frequency")
+    plt.xlabel("Node Degree")
+    plt.xlim(0,100)
+    plt.show()
+    # Print to PDF
+    figfile = os.path.join(os.getcwd(),'fig',str(root)+"node_degree_ECDF")
+    fig.savefig(figfile, format='pdf')
+
+
+#def plot2PDF(figure,name):
+
+
+
+if False:
+    nodeD = [n.totalLinks for n in g.nodes.values()]
+    data = Counter(nodeD).items()
+    data = sorted(data,key=lambda x: x[0])
+    total = len(g.nodes)
+
+    x = [degree for (degree, freq) in data]
+    y = [freq for (degree,freq) in data]
+    #y = np.cumsum(y)
+
+    plt.title("Node Degree CDF")
+    plt.ylabel("Frequency")
+    plt.xlabel("Total Number of Proxies")
+
+    plt.scatter(x, y)
+    plt.show()
+
+
+#def  typesBars(data):
+
+
+def creationDate():
     # For now it works for proxies
     # All creation dates
-    proxies = [(dt.datetime.strptime(p.created, '%Y%m%d %I%M')).date() for p in g.services.values() if p.type=='Proxy']
+    #proxies = [(dt.datetime.strptime(p.created, '%Y%m%d %I%M')).date() for p in g.services.values() if p.type=='Proxy']
+    proxies = [(dt.datetime.strptime(p.created, '%Y%m%d %I%M')) for p in g.services.values() if p.type=='Proxy']
 
 
     # Tuple of date, no of occurences
@@ -48,13 +118,21 @@ def creationDateECDF():
     #total = y[-1]
     #y = [temp/total for temp in y ]
 
+    # Initilaze figure to print later
+    fig = plt.Figure()
+    fig.set_canvas(plt.gcf().canvas)
     plt.plot_date(x=x, y=y)
-    plt.title("Page impressions on example.com")
-    plt.ylabel("Page impressions")
+    # Set graph parameters
+    plt.title("Evolution of proxy service creation")
+    plt.ylabel("Total Number of Proxies")
     #plt.grid(True)
     #plt.ylim((0,3))
     #plt.step(x, y)
     plt.show()
+    # Print to PDF
+    figfile = os.path.join(os.getcwd(),'fig',str(root)+"proxy_creation")
+    fig.savefig(figfile, format='pdf')
+
 
 
     if False:
@@ -114,3 +192,6 @@ def creationDateECDF():
         fig.autofmt_xdate()
 
         plt.show()
+
+#nodeDegreeECDF()
+#creationDate()
