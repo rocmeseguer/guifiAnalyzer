d16de512a8dab06d0308e2fbf504a2eadcff6dfc
Manos <savemanos@gmail.com>
2015-09-22 03:40:30 +0200

    Reaching the final form of parallel requests. Have to test it

--- traffic/testParallel.py
+++ traffic/testParallel.py
@@ -16,6 +16,101 @@ services = [
     "stats"]
 
 
+def buildArgs(args):
+    """
+    Take a list of arguments and convert it to a url
+    string
+    """
+    arguments = ""
+    for arg in args:
+        arguments += "&" + str(arg)
+        if args[arg]:
+            arguments += "="
+            for i in args[arg]:
+                if i == args[arg][-1]:
+                    #print i
+                    arguments += str(i)
+                else:
+                    # print i
+                    arguments += str(i) + ","
+    return arguments
+
+def doRequest(base, arguments, timeout):
+    # Make request
+    url = base + arguments
+    print len(arguments)
+    #print("SNPServices request to graph server: %s" % url)
+    req = urllib2.Request(url)
+    if timeout:
+        return urllib2.urlopen(req, timeout=timeout)
+    else:
+        return urllib2.urlopen(req)
+
+
+
+
+def doParallelRequests(base, args, timeout, csv):
+    # Define utilites for parallel request
+    from urlparse import urlparse
+    from threading import Thread
+    import httplib, sys
+    from Queue import Queue
+
+    def buildUrls(base, argsLists):
+        argumentsList = map(buildArgs,argsList)
+        urlsList = [(base+argument) for argument in argumentsList]
+        return urlsList
+
+    def chunks(l, n):
+        """
+        Yield successive n-sized chunks from l.
+        """
+        for i in xrange(0, len(l), n):
+            yield l[i:i+n]
+    
+
+    def worker():
+        while True:
+            # Parallel stuff start under this command
+            url = q.get()
+            req = urllib2.Request(url)
+            if timeout:
+                responses.extend(urllib2.urlopen(req, timeout=timeout))
+            else:
+                responses.extend(urllib2.urlopen(req))
+            q.task_done()
+             # Parallel stuff end above this command
+
+    
+
+    # Separate arguments in lists that fit the apache max url size (4000 chars)
+    chunksize = (len(args)*6) / (4000-len(base))
+    argsLists  = list(chunks(args, chunksize))
+    urls = [buildUrls(base,argsList) for argsList in argsLists]
+    print urls
+    #Do parallel request for each url innlist
+    concurrent = len(urls)
+    q = Queue(concurrent * 2)
+    responses = []
+
+    for i in range(concurrent):
+        t = Thread(target=worker)
+        t.daemon = True
+        t.start()
+    try:
+        for url in urls:
+            q.put(url.strip())
+        q.join()
+
+    if csv:
+        # TODO
+        #Merge all responses in one csv... How?????
+        return response
+    else: 
+        readResponses = [r.read() for r in responses]
+        return readResponses
+
+
 def snpRequest(ip, command="help", args={}, debug=False, timeout=0, csv = False):
     """
     Request to snpservices server
@@ -30,39 +125,16 @@ def snpRequest(ip, command="help", args={}, debug=False, timeout=0, csv = False)
     else:
 
         base = "http://" + str(ip) + "/snpservices/index.php?call=" + command
-        # Add arguments
-        arguments = ""
-        for arg in args:
-            arguments += "&" + str(arg)
-            if args[arg]:
-                arguments += "="
-                for i in args[arg]:
-                    if i == args[arg][-1]:
-                        #print i
-                        arguments += str(i)
-                    else:
-                        # print i
-                        arguments += str(i) + ","
+        # Build arguments
+        arguments = buildArgs(args)
+        
         # New code
         # Url character Limit
         # http://stackoverflow.com/a/1289610
-        if arguments < 4000:
-            doRequest(base, arguments, timeout)
+        if len(arguments) < 4000:
+            response = doRequest(base, arguments, timeout)
         else:
-            doParallellRequests(base,args, timeout)
-
-        # Make request
-        url = base + arguments
-        print len(arguments)
-        #print("SNPServices request to graph server: %s" % url)
-        req = urllib2.Request(url)
-        #if timeout:
-        #   response = urllib2.urlopen(req, timeout=timeout)
-        #else:
-        #    response = urllib2.urlopen(req)
-        # New code
-        response = doRequest(req,timeout)
-
+            response = doParallellRequests(base, args, timeout, csv)
 
         if csv:
             return response
@@ -72,99 +144,5 @@ def snpRequest(ip, command="help", args={}, debug=False, timeout=0, csv = False)
 
 #/snpservices/index.php?call=stats&devices=
 
-
-def doRequest(base, arguments, timeout):
-    # Make request
-    url = base + arguments
-    print len(arguments)
-    #print("SNPServices request to graph server: %s" % url)
-    req = urllib2.Request(url)
-    if timeout:
-        return urllib2.urlopen(req, timeout=timeout)
-    else:
-        return urllib2.urlopen(req)
-
-
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
-
-def doParallelRequests(base, arguments, timeout):
-    arglists  = list(chunks(arguments,))
-    for arg in args:
-        arguments += "&" + str(arg)
-        if args[arg]:
-            arguments += "="
-            for i in args[arg]:
-                if i == args[arg][-1]:
-                    #print i
-                    arguments += str(i)
-                else:
-                    # print i
-                    arguments += str(i) + ","
-    # Make request
-    url = base + arguments
-    print len(arguments)
-    #print("SNPServices request to graph server: %s" % url)
-    req = urllib2.Request(url)
-    if timeout:
-        return urllib2.urlopen(req, timeout=timeout)
-    else:
-        return urllib2.urlopen(req)
-
-# Define utilites for parallel request
-from urlparse import urlparse
-from threading import Thread
-import httplib, sys
-from Queue import Queue
-
-concurrent = 200
-
-def doWork():
-    while True:
-        url = q.get()
-        status, url = getStatus(url)
-        doSomethingWithResult(status, url)
-        q.task_done()
-
-def getStatus(ourl):
-    try:
-        url = urlparse(ourl)
-        conn = httplib.HTTPConnection(url.netloc)
-        conn.request("HEAD", url.path)
-        res = conn.getresponse()
-        return res.status, ourl
-    except:
-        return "error", ourl
-
-def doSomethingWithResult(status, url):
-    print status, url
-
-
-
-# 10k/max_numero_ids_por_url/max_concurrent
-
-def doParallelRequest(request, timeout):
-    q = Queue(concurrent * 2)
-    for i in range(concurrent):
-        t = Thread(target=doWork)
-        t.daemon = True
-        t.start()
-    try:
-        for url in open('urllist.txt'):
-            q.put(url.strip())
-        q.join()
-    except KeyboardInterrupt:
-        sys.exit(1)
-
-
-
-
-
-     if timeout:
-            return urllib2.urlopen(req, timeout=timeout)
-        else:
-            return urllib2.urlopen(req)
 
 
