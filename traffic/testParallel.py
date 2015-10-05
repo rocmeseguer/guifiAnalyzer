c7cb60ae0b9bc18dee7e58c1ac4e666caaa8f01a
Manos <savemanos@gmail.com>
2015-10-05 14:24:40 +0200

    Various issues

--- traffic/testParallel.py
+++ traffic/testParallel.py
@@ -51,12 +51,14 @@ def doRequest(base, arguments, timeout):
 
 
 
-def doParallelStatsRequests(base, args, timeout, csv):
+def doParallelStatsRequests(base, args, tout, csv):
     # Define utilites for parallel request
     from urlparse import urlparse
     from threading import Thread
     import httplib, sys
     from Queue import Queue
+
+    timeout = tout
 
     def buildUrl(base, devices):
         url = base
@@ -67,7 +69,7 @@ def doParallelStatsRequests(base, args, timeout, csv):
             else:
                 # print i
                 url += str(i) + ","
-        return url
+        return url 
 
     def chunks(l, n):
         """
@@ -80,11 +82,32 @@ def doParallelStatsRequests(base, args, timeout, csv):
         while True:
             # Parallel stuff start under this command
             url = q.get()
+            print "%s\n" % url
             req = urllib2.Request(url)
             if timeout:
-                responses.extend(urllib2.urlopen(req, timeout=timeout))
+                while True:
+                    try:
+                        response = urllib2.urlopen(req, timeout=timeout)
+                        responses.extend(response)
+                        print "Done"
+                    except urllib2.HTTPError as e:
+                        print e
+                    except urllib2.URLError as e:
+                        print e
+                    break
+                #pass
             else:
-                responses.extend(urllib2.urlopen(req))
+                while True:
+                    try:
+                        response = urllib2.urlopen(req)
+                        responses.extend(response)
+                        print "Done"
+                    except urllib2.HTTPError as e:
+                        print e
+                    except urllib2.URLError as e:
+                        print e
+                    break
+                #pass
             q.task_done()
              # Parallel stuff end above this command
 
@@ -97,11 +120,19 @@ def doParallelStatsRequests(base, args, timeout, csv):
 
     # Separate arguments in lists that fit the apache max url size (4000 chars)
     # Each arg has a max of 5 chars + the comma after it
-    chunksize = (len(devices)*6) / (4000-len(base))
+    #WRONG chunksize = (len(devices)*6) / (4000-len(base))
+    print "total devices"
+    print len(devices)
+    chunksize = (100-len(base))/6
+    print "chunksize (# of devices)"
     print chunksize
     devicesLists  = list(chunks(devices, chunksize))
+    print "devicesLists"
+    print len(devicesLists)
     urls = [buildUrl(base,devicesList) for devicesList in devicesLists]
-    print urls
+    #print "Urls"
+    urlsLen = [len(url) for url in urls]
+    print urlsLen
     #Do parallel request for each url innlist
     concurrent = len(urls)
     q = Queue(concurrent * 2)
@@ -118,13 +149,24 @@ def doParallelStatsRequests(base, args, timeout, csv):
     except KeyboardInterrupt:
         sys.exit(1)
 
-    if csv:
+
+    with open('test.txt','a') as outfile:
+        for response in responses:
+            outfile.write(response)
+            #reader = response.reader()
+            #for row in reader:
+            #    outfile.write(row)
+
+
+    # Queue problem????
+
+    #if csv:
         # TODO
         #Merge all responses in one csv... How?????
-        return response
-    else:
-        readResponses = [r.read() for r in responses]
-        return readResponses
+    #    return response
+    #else:
+    #    readResponses = [r.read() for r in responses]
+    #    return readResponses
 
 
 def snpRequest(ip, command="help", args={}, debug=False, timeout=0, csv = False):
@@ -143,7 +185,8 @@ def snpRequest(ip, command="help", args={}, debug=False, timeout=0, csv = False)
         base = "http://" + str(ip) + "/snpservices/index.php?call=" + command
         # Build arguments
         arguments = buildArgs(args)
-        print arguments
+        #print "Total Args" 
+        #print len(arguments)
 
 
         # New code
@@ -156,25 +199,33 @@ def snpRequest(ip, command="help", args={}, debug=False, timeout=0, csv = False)
             # Only stats command can have multiple argument values (for devices)
             # index.php?call=stats&devices=<device_id>[,<device_id>]
             print "Parallel Request"
-            response = doParallellStatsRequests(base, args, timeout, csv)
+            response = doParallelStatsRequests(base, args, timeout, csv)
 
-        if csv:
-            return response
-        else:
-            data = response.read()
-            return data
+        #if csv:
+        #    return response
+        #else:
+        #    data = response.read()
+        #    return data
 
 #/snpservices/index.php?call=stats&devices=
+
+def snpGetDevices(ip, devices):
+    url = "http://" + str(ip) + "/snpservices/index.php?call=stats"
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    #with open('test.txt','a') as outfile:
+    #    outfile.write(response)
+
 
 
 
 #TEST
 #from guifiAnalyzer.traffic import testParallel as par
 #from guifiAnalyzer.traffic import graphDevicesInfo as gd
-#gd.graphDevicesInfo(8346,False)
-#linksTable,devicesTable, graphServersTable = gd.loadDB(8346, False)
-#toBeGraphed = [int(d1) for d1,d2 in devicesTable.iteritems() if str(d2['graphServer']) == '9969']
-#b={'devices':ltoBeGraphed}
-#a = par.snpRequest("perafita.guifi.net","stats",b,0,False)
+#gd.graphDevicesInfo(18668,False)
+#linksTable,devicesTable, graphServersTable = gd.loadDB(18668, False)
+#toBeGraphed = [int(d1) for d1,d2 in devicesTable.iteritems() if str(d2['graphServer']) == '21314']
+#b={'devices':toBeGraphed}
+#a = par.snpRequest("castello.guifi.net","stats",b,0,False)
 #OR
 #a = par.doParallelStatsRequests("http://perafita.guifi.net/snpservices/index.php?call=stats",b,0,False)
